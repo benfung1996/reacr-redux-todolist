dcf478c271dc6920e117b3393372d2475df0e9c5
Ben Fung <benfung1996@gmail.com>
2020-07-22 21:10:00 -0700

    added reducer

--- react-redux-todolist/src/reducers/index.js
+++ react-redux-todolist/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import todoReducer from './todoReducer';
+import visibilityFilter from './filterReducer'
 
 export default combineReducers({
-  todolist: todoReducer
+  todoReducer,
+  visibilityFilter
 });
---
+++ react-redux-todolist/src/reducers/todoReducer.js
@@ -0,0 +1,36 @@
+import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO } from '../actions/types';
+import uuid from 'uuidv4';
+
+const INITIAL_DATA = [{
+    id: uuid,
+    text: 'TEST1',
+},
+{
+    id: uuid,
+    text: 'TEST2'
+}
+]
+
+const todoReducer = (state = INITIAL_DATA, action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ]
+        case TOGGLE_TODO:
+            return state.map(todo =>
+                (todo.id === action.id) ? { ...todo, completed: !todo.completed } : todo
+            )
+        case REMOVE_TODO:
+            return state.filter(todo => todo.id !== action.id);
+        default:
+            return state
+    }
+}
+
+export default todoReducer;
