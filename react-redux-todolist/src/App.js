32616290a70448a87f444bd042a857df3be52cc8
Ben Fung <benfung1996@gmail.com>
2020-07-22 21:58:33 -0700

    added todo.js and todoList.js

--- react-redux-todolist/src/App.js
+++ react-redux-todolist/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
-
 import TodoList from './components/TodoList/TodoList';
+import Todo from './components/Todo/Todo';
 
 export default class App extends Component {
   render() {
     return (
      <Provider store={store}>
        <TodoList />
+       <Todo />
      </Provider>
     )
   }
